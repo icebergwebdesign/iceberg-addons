0dc9de88b42ef0f5b55341f5cbfacc69aca810d3
zenzroid <dev@zenzro.id>
2024-04-04 22:07:24 +0700

    Added Iceberg Custom Elements
    Added Iceberg Custom Elements to git updater setting

--- includes/git-updater-setting.php
+++ includes/git-updater-setting.php
@@ -14,6 +14,7 @@ add_filter( 'gu_set_options',
         return array( 
             'github_access_token' => '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',
             'iceberg-addons'    => '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',
+            'iceberg-custom-elements'    => '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',
             'custom-swatches-for-iris-color-picker'    => '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',
         );
     } );
