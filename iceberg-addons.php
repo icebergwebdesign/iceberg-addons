3c803635fd3416e5c4e59fc8cad0428034f8e58a
zenzroid <dev@zenzro.id>
2024-04-20 06:42:28 +0700

    update token
    update token using support github classic

--- iceberg-addons.php
+++ iceberg-addons.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Iceberg AddOns
  * Plugin URI:        https://www.icebergwebdesign.com/
  * Description:       Iceberg AddOns
- * Version:           1.2.2
+ * Version:           1.2.3
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            Iceberg Web Design
