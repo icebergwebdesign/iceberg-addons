0f36fe30f59946f48b534f1b0489df98c5aabcf8
zenzroid <dev@zenzro.id>
2024-04-23 03:02:36 +0700

    update to latest personal token public3
    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

--- includes/git-updater-setting.php
+++ includes/git-updater-setting.php
@@ -12,9 +12,9 @@
 add_filter( 'gu_set_options',
     function () {
         return array( 
-            'github_access_token' => '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',
-            'iceberg-addons'    => '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',
-            'iceberg-custom-elements'    => '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',
-            'custom-swatches-for-iris-color-picker'    => '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',
+            'github_access_token' => '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',
+            'iceberg-addons'    => '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',
+            'iceberg-custom-elements'    => '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',
+            'custom-swatches-for-iris-color-picker'    => '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',
         );
     } );
