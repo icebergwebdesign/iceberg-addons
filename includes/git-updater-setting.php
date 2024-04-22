ae1a61f5b9479ff2aa36d19ae0a27e5b13db7c5b
zenzroid <dev@zenzro.id>
2024-04-22 23:45:56 +0700

    revert back to old access token

--- includes/git-updater-setting.php
+++ includes/git-updater-setting.php
@@ -12,9 +12,9 @@
 add_filter( 'gu_set_options',
     function () {
         return array( 
-            'github_access_token' => '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',
-            'iceberg-addons'    => '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',
-            'iceberg-custom-elements'    => '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',
-            'custom-swatches-for-iris-color-picker'    => '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',
+            'github_access_token' => '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',
+            'iceberg-addons'    => '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',
+            'iceberg-custom-elements'    => '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',
+            'custom-swatches-for-iris-color-picker'    => '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',
         );
     } );
